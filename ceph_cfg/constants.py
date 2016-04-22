027fdbc831fdfd6739acb8e690645c0fc7d5938c
Owen Synge <osynge@suse.com>
2016-04-22 11:44:35 +0200

    ceph_cfg.constants: Add constants for mds and rgw services
    We need to kwow the path to the mds and rgw services.
    
    Signed-off-by: Owen Synge <osynge@suse.com>

--- ceph_cfg/constants.py
+++ ceph_cfg/constants.py
@@ -13,6 +13,8 @@ _path_sgdisk = find_executable('sgdisk')
 _path_ceph_authtool = find_executable('ceph-authtool')
 _path_ceph = find_executable('ceph')
 _path_ceph_mon = find_executable('ceph-mon')
+_path_ceph_rgw = find_executable('radosgw')
+_path_ceph_mds = find_executable('ceph-mds')
 _path_systemctl = find_executable('systemctl')
 
 JOURNAL_UUID = '45b0969e-9b03-4f30-b4c6-b4b80ceff106'
