2712a2859fff79ce8d891068a4580778497e6a70
Owen Synge <osynge@suse.com>
2016-07-08 12:18:04 +0200

    ceph_cfg.init: Remove salt invocation
    Since this is a library we dont need salt CLI examples.
    
    Signed-off-by: Owen Synge <osynge@suse.com>

--- ceph_cfg/__init__.py
+++ ceph_cfg/__init__.py
@@ -570,20 +570,13 @@ def keyring_mds_purge(**kwargs):
 
 def keyring_rgw_create(**kwargs):
     """
-    Create rgw keyring for cluster
+    Create rgw bootstrap keyring for cluster.
 
-    CLI Example:
-
-        salt '*' sesceph.keyring_rgw_create \\
-                'cluster_name'='ceph' \\
-                'cluster_uuid'='cluster_uuid'
-    Notes:
-
-    cluster_uuid
-        Set the cluster UUID. Defaults to value found in ceph config file.
-
-    cluster_name
-        Set the cluster name. Defaults to "ceph".
+    Args:
+        **kwargs: Arbitrary keyword arguments.
+            cluster_uuid : Set the cluster UUID. Defaults to value found in
+                ceph config file.
+            cluster_name : Set the cluster name. Defaults to "ceph".
     """
     params = dict(kwargs)
     params["keyring_type"] = "rgw"
