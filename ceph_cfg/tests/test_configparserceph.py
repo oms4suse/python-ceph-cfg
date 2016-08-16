0aea3928324c5888c42b8238687652f91e50611d
Owen Synge <osynge@suse.com>
2016-08-16 15:48:05 +0200

    ceph_cfg.tests.test_configparserceph: python3 support
    io.BytesIO is a bit different in python3 so changing way this code works.
    
    Signed-off-by: Owen Synge <osynge@suse.com>

--- ceph_cfg/tests/test_configparserceph.py
+++ ceph_cfg/tests/test_configparserceph.py
@@ -1,46 +1,63 @@
 from ceph_cfg.util_configparser import ConfigParserCeph as ConfigParser
-import io
+import tempfile
+import os.path
+import shutil
+
 
 class Test_util_configparser(object):
+    def setup(self):
+        """
+        Make temp directory for tests.
+        """
+        self.test_dir = tempfile.mkdtemp()
 
+
+    def teardown(self):
+        """
+        Remove temp directory and content
+        """
+        shutil.rmtree(self.test_dir)
+
+        
     def test_whitespace_none(self):
-        sample_config = """
-[mysqld]
-user2 = mysql
-"""
         config = ConfigParser()
-        config.readfp(io.BytesIO(sample_config))
+        file_name = os.path.join(self.test_dir,"file")
+        with open(file_name, 'wt') as fp:
+            fp.write("[mysqld]\n")
+            fp.write("user2 = mysql")
+        config.read(file_name)
         value = config.get("mysqld", "user2")
         assert value == "mysql"
 
 
     def test_whitespace(self):
-        sample_config = """
-[mysqld]
-user 2 = mysql
-"""
         config = ConfigParser()
-        config.readfp(io.BytesIO(sample_config))
+        file_name = os.path.join(self.test_dir,"file")
+        with open(file_name, 'wt') as fp:
+            fp.write("[mysqld]\n")
+            fp.write("user 2 = mysql\n")
+        config.read(file_name)
         value = config.get("mysqld", "user_2")
         assert value == "mysql"
 
+
     def test_whitespace_two(self):
-        sample_config = """
-[mysqld]
-user 2 3 = mysql
-"""
         config = ConfigParser()
-        config.readfp(io.BytesIO(sample_config))
+        file_name = os.path.join(self.test_dir,"file")
+        with open(file_name, 'wt') as fp:
+            fp.write("[mysqld]\n")
+            fp.write("user 2 3 = mysql\n")
+        config.read(file_name)
         value = config.get("mysqld", "user_2_3")
         assert value == "mysql"
 
 
     def test_whitespace_as_underscore(self):
-        sample_config = """
-[mysqld]
-user_2 = mysql
-"""
         config = ConfigParser()
-        config.readfp(io.BytesIO(sample_config))
+        file_name = os.path.join(self.test_dir,"file")
+        with open(file_name, 'wt') as fp:
+            fp.write("[mysqld]\n")
+            fp.write("user_2 = mysql\n")
+        config.read(file_name)
         value = config.get("mysqld", "user_2")
         assert value == "mysql"
