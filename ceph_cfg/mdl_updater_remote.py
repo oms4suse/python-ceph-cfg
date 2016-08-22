4320ae8996755569900a15ebed1b50d84e9fb6fa
Owen Synge <osynge@suse.com>
2016-08-22 17:41:06 +0200

    ceph_cfg.mdl_updater_remote: remove functions now in ops_auth
    Move funtions in ops_auth from ceph_cfg.mdl_updater_remote
    
    We have an issue connection keys being reestablished if we have more than one
    object that makes remote connections so time to reactor this into multiple
    components and store the data in the model.
    
    Signed-off-by: Owen Synge <osynge@suse.com>

--- ceph_cfg/mdl_updater_remote.py
+++ ceph_cfg/mdl_updater_remote.py
@@ -5,12 +5,11 @@ from __future__ import absolute_import
 import logging
 import os
 import json
-import shlex
+
 
 # Local imports
 from . import keyring
 from . import utils
-from . import mdl_query
 from . import util_which
 from . import constants
 
@@ -111,115 +110,3 @@ class model_updater_remote():
                         output["stderr"])
                         )
         self.model.cluster_status = json.loads(output["stdout"].strip())
-
-
-    def auth_list(self):
-        prefix_arguments = [
-            util_which.which_ceph.path
-        ]
-        postfix_arguments = [
-            "auth",
-            "list"
-            ]
-        connection_arguments = self.connection_arguments_get()
-        arguments = prefix_arguments + connection_arguments + postfix_arguments
-        output = utils.execute_local_command(arguments)
-        if output["retcode"] != 0:
-            raise Error("Failed executing '%s' Error rc=%s, stdout=%s stderr=%s" % (
-                        " ".join(arguments),
-                        output["retcode"],
-                        output["stdout"],
-                        output["stderr"])
-                        )
-        auth_list_out = {}
-        section = {}
-        for line in output["stdout"].split('\n'):
-            if len(line) == 0:
-                continue
-            if line[0] != '\t':
-                prev_sec_name = section.get("name")
-                if prev_sec_name is not None:
-                    auth_list_out[prev_sec_name] = section
-                section = { "name" : line }
-                continue
-            tokenised_line = shlex.split(line)
-            if len(tokenised_line) == 0:
-                continue
-            if tokenised_line[0] == 'key:':
-                section['key'] = tokenised_line[1]
-            if tokenised_line[0] == 'caps:':
-                if not 'caps' in section:
-                    section['caps'] = []
-                cap_details = tokenised_line[1:]
-                section["caps"].append(cap_details)
-
-
-        prev_sec_name = section.get("name")
-        if prev_sec_name is not None:
-            auth_list_out[prev_sec_name] = section
-        self.model.auth_list = auth_list_out
-
-
-    def auth_add(self, keyring_type):
-        """
-        Authorise keyring
-        """
-        keyringobj = keyring.keyring_facard(self.model)
-        keyringobj.key_type = keyring_type
-
-
-        if not keyringobj.present():
-            raise Error("rgw keyring not found")
-        q = mdl_query.mdl_query(self.model)
-        if q.mon_is() and q.mon_quorum() is False:
-            raise Error("mon daemon is not in quorum")
-        prefix_arguments = [
-            util_which.which_ceph.path
-        ]
-        postfix_arguments = [
-            "auth",
-            "import",
-            "-i",
-            keyringobj.keyring_path_get()
-            ]
-        connection_arguments = self.connection_arguments_get()
-        arguments = prefix_arguments + connection_arguments + postfix_arguments
-        output = utils.execute_local_command(arguments)
-        if output["retcode"] != 0:
-            raise Error("Failed executing '%s' Error rc=%s, stdout=%s stderr=%s" % (
-                        " ".join(arguments),
-                        output["retcode"],
-                        output["stdout"],
-                        output["stderr"])
-                        )
-        return True
-
-
-    def auth_del(self, keyring_type):
-        """
-        Remove Authorised keyring
-        """
-        keyringobj = keyring.keyring_facard(self.model)
-        keyringobj.key_type = keyring_type
-        q = mdl_query.mdl_query(self.model)
-        if q.mon_is() and q.mon_quorum() is False:
-            raise Error("mon daemon is not in quorum")
-        prefix_arguments = [
-            util_which.which_ceph.path
-        ]
-        postfix_arguments = [
-            "auth",
-            "del",
-            keyringobj.keyring_identity_get()
-            ]
-        connection_arguments = self.connection_arguments_get()
-        arguments = prefix_arguments + connection_arguments + postfix_arguments
-        output = utils.execute_local_command(arguments)
-        if output["retcode"] != 0:
-            raise Error("Failed executing '%s' Error rc=%s, stdout=%s stderr=%s" % (
-                        " ".join(arguments),
-                        output["retcode"],
-                        output["stdout"],
-                        output["stderr"])
-                        )
-        return True
