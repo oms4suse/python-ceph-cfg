e33379740d4a148bae0be46e84d647898b90ed7c
Owen Synge <osynge@suse.com>
2016-11-11 18:18:26 +0100

    ceph_cfg.util_configparser : ceph can have no section header
    With ceph config files they can have no section header and this is by
    default the "global" section. Make the ConfigParser do this rather than
    throw the ConfigParser.MissingSectionHeaderError exception.
    
    Signed-off-by: Owen Synge <osynge@suse.com>

--- ceph_cfg/util_configparser.py
+++ ceph_cfg/util_configparser.py
@@ -1,3 +1,4 @@
+from io import StringIO
 try:
     import ConfigParser
 except:
@@ -14,3 +15,36 @@ class ConfigParserCeph(ConfigParser.ConfigParser):
         replaced = stripped.replace(' ', '_')
         return replaced
 
+
+    def _MissingSectionHeaderError_read(self,file_name):
+        vfile = StringIO(u'[global]\n%s' % open(file_name).read())
+        return ConfigParser.ConfigParser.readfp(self, vfile)
+
+
+    def read(self, filenames):
+        """Read and parse a filename or a list of filenames.
+
+        Files that cannot be opened are silently ignored; this is
+        designed so that you can specify a list of potential
+        configuration file locations (e.g. current directory, user's
+        home directory, systemwide directory), and all existing
+        configuration files in the list will be read.  A single
+        filename may also be given.
+
+        Return list of successfully read files.
+        """
+        if isinstance(filenames, ("".__class__, u"".__class__)):
+            filenames = [filenames]
+        read_ok = []
+        for filename in filenames:
+            try:
+                fp = open(filename)
+            except IOError:
+                continue
+            try:
+                self._read(fp, filename)
+            except ConfigParser.MissingSectionHeaderError:
+                self._MissingSectionHeaderError_read(filename)
+            fp.close()
+            read_ok.append(filename)
+        return read_ok
